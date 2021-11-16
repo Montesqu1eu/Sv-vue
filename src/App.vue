e362d8ad6b8029cd1dd383d8ae0a79b6b3842198
Montesqu1eu <ildar.golden@gmail.com>
2021-11-16 15:17:24 +0300

    4.4

--- src/App.vue
+++ src/App.vue
@@ -2,82 +2,7 @@
   <div id="app">
     <section class="catalog">
       <product-list :products="products"/>
-      <ul class="catalog__pagination pagination">
-        <li class="pagination__item">
-          <a
-            class="pagination__link pagination__link--arrow pagination__link--disabled"
-            aria-label="Предыдущая страница"
-          >
-            <svg
-              width="8"
-              height="14"
-              fill="currentColor"
-            >
-              <use xlink:href="#icon-arrow-left"/>
-            </svg>
-          </a>
-        </li>
-        <li class="pagination__item">
-          <a class="pagination__link pagination__link--current">
-            1
-          </a>
-        </li>
-        <li class="pagination__item">
-          <a
-            class="pagination__link"
-            href="#"
-          >
-            2
-          </a>
-        </li>
-        <li class="pagination__item">
-          <a
-            class="pagination__link"
-            href="#"
-          >
-            3
-          </a>
-        </li>
-        <li class="pagination__item">
-          <a
-            class="pagination__link"
-            href="#"
-          >
-            4
-          </a>
-        </li>
-        <li class="pagination__item">
-          <a
-            class="pagination__link"
-            href="#"
-          >
-            ...
-          </a>
-        </li>
-        <li class="pagination__item">
-          <a
-            class="pagination__link"
-            href="#"
-          >
-            10
-          </a>
-        </li>
-        <li class="pagination__item">
-          <a
-            class="pagination__link pagination__link--arrow"
-            href="#"
-            aria-label="Следующая страница"
-          >
-            <svg
-              width="8"
-              height="14"
-              fill="currentColor"
-            >
-              <use xlink:href="#icon-arrow-right"/>
-            </svg>
-          </a>
-        </li>
-      </ul>
+      <base-pagination v-model="page" :count="countProducts" :per-page="productsPerPage"/>
     </section>
   </div>
 </template>
@@ -85,16 +10,29 @@
 <script>
 import ProductList from '@/components/ProductList.vue';
 import products from './data/products';
+import BasePagination from '@/components/BasePagination.vue';
 
 export default {
   components: {
     ProductList,
+    BasePagination,
   },
   name: 'App',
   data() {
     return {
-      products,
+      page: 1,
+      productsPerPage: 3,
     };
+  },
+  computed: {
+    products() {
+      const offset = (this.page - 1) * this.productsPerPage;
+
+      return products.slice(offset, offset + this.productsPerPage);
+    },
+    countProducts() {
+      return products.length;
+    },
   },
 };
 </script>
